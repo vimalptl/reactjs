c4af90918151402349c02f0ce03239eb45d4d87f
Patel <vimalp@ri-net.com>
2018-11-14 08:52:00 -0800

    Update defaults names with project name.

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# My Portfolio
+This is my React Portfolio
+## This is my work.
+AuthorQuiz - Learning React from PruralSight - React Fundamentals
---
+++ authorquiz/src/AuthorQuiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './AuthorQuiz.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div className="App">
+        AuthorQuiz Application
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
