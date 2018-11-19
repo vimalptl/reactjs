d6163264bfd6af635c882a43a0e1373d87faa461
Patel <vimalp@ri-net.com>
2018-11-18 17:51:00 -0800

    Select Answer with color red / green

--- authorquiz/src/index.js
+++ authorquiz/src/index.js
@@ -62,8 +62,19 @@ function getTurnData(authors) {
 }
 
 const state = {
-    turnData: getTurnData(authors)
+    turnData: getTurnData(authors),
+    highlight: ''
 };
 
-ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
+function onAnswerSelected(answer) {
+    const isCorrect = state.turnData.author.books.some((book) => book === answer);
+    state.highlight = isCorrect ? 'correct' : 'wrong';
+    render();
+}
+
+function render() {
+  ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>, document.getElementById('root'));
+}
+
+render();
 ServiceWorker.unregister();
