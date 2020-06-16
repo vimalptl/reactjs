629a79af813392021ff913c4a045dfda1e21f49e
Patel <vimalp@ri-net.com>
2020-06-15 17:13:18 -0700

    Add New Author

--- authorquiz/src/AuthorQuiz.js
+++ authorquiz/src/AuthorQuiz.js
@@ -1,54 +1,114 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './AuthorQuiz.css';
-import './bootstrap.min.css';
+import "./custom.scss";
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function Hero() {
-  return (<div className="row">
-    <div className="jumbotron col-10 offset-1">
-      <h1>Author Quiz</h1>
-      <p>Select the book written by the author shown</p>
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book writtern by the author shown </p>
+      </div>
     </div>
-  </div>);
+  );
 }
 
-function Book({title}){
-  return (<div className="answer">
-    <h4>{title}</h4>
-  </div>);
+function Book({ title, onClick }) {
+  return (
+    <div
+      className="answer"
+      onClick={() => {
+        onClick(title);
+      }}
+    >
+      <h4>{title}</h4>
+    </div>
+  );
 }
 
-function Turn({author, books}) {
-  return (<div className="row turn" style={{backgroundColor: "white"}}>
-    <div className="col-4 offset-1">
-      <img src={author.imageUrl} className="authorimage" alt="Author"/>
-    </div> 
-    <div className="col-6">
-      {books.map((title) => <Book title={title} key={title}/>)}
+function Turn({ author, books, highlight, onAnswerSelected }) {
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      none: "",
+      correct: "green",
+      wrong: "red",
+    };
+    return mapping[highlight];
+  }
+  return (
+    <div
+      className="row turn"
+      style={{ backgroundColor: highlightToBgColor(highlight) }}
+    >
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author" />
+      </div>
+      <div className="col-6">
+        {books.map((title) => (
+          <Book title={title} key={title} onClick={onAnswerSelected} />
+        ))}
+      </div>
     </div>
-  </div>)
+  );
 }
-function Continue() {
-  return (<div></div>);
+
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired,
+};
+
+function Continue({ show, onContinue }) {
+  return (
+    <div className="row continue">
+      {show ? (
+        <div className="col-11">
+          <button
+            className="btn btn-primary btn-lg float-right"
+            onClick={onContinue}
+          >
+            Continue
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 function Footer() {
-  return (<div id="footer" className="row">
-    <div className="col-12">
-      <p className="text-muted credit">All images are from <a href="https://commons.wikimedia.org/wiki/Main_Page">Wikimedia Commons</a> and are in the public domain</p>
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted creadit">All images are from ???</p>
+      </div>
     </div>
-  </div>);
+  );
 }
 
-function AuthorQuiz({turnData}) {
-    return (
-      <div className="container-fluid">
-        <Hero/>
-        <Turn {...turnData} />
-        <Continue/>
-        <Footer/>
-      </div>
-    );
+function AuthorQuiz({ turnData, highlight, onAnswerSelected, onContinue }) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn
+        {...turnData}
+        highlight={highlight}
+        onAnswerSelected={onAnswerSelected}
+      />
+      <Continue show={highlight === "correct"} onContinue={onContinue} />
+      <p>
+        <Link to="/add">Add Author</Link>
+      </p>
+      <Footer />
+    </div>
+  );
 }
 
 export default AuthorQuiz;
--- authorquiz/src/AuthorQuiz.test.js
+++ authorquiz/src/AuthorQuiz.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './AuthorQuiz';
+import React from "react";
+import ReactDOM from "react-dom";
+import AuthorQuiz from "./AuthorQuiz";
+import Enzyme, { mount, shallow, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AuthorQuiz />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+const state = {
+  turnData: {
+    books: ["The shining"],
+    author: {
+      name: "Charles Dickens",
+      imageUrl: "images/authors/charlesdickens.jpg",
+      imageSource: "WikiSource",
+      books: ["David Coperfield", "A tale of two cities"],
+    },
+  },
+  highlight: "none",
+};
+
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => {}} />, div);
+  });
 });
